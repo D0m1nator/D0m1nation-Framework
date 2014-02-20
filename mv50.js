2cb1ed03e16d3d67904e2b0aa3521a23a52f3a7a
D0m1nator <expirioner@gmail.com>
2014-02-20 14:38:56 +0700

    .idea excluded

---
+++ mv50.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Konstantin on 2/20/14.
+ */
+var strYttrium='Yelement',
+    strVanadium='Vanadium';
+    funAlchemy=function(elemToTransmutate1, elemToTransmutate2){
+       return elemToTransmutate1 + elemToTransmutate2;
+    };
+
+
--- nitrogen.js
+++ nitrogen.js
@@ -1,12 +1,2 @@
-/**
- * Created by Konstantin on 2/20/14.
- */
-var nitrogen1,
-    nitrogen2,
-    nitrogen3;
-function nitrogenAddler(nitrogen1, nnitrogen2,nitrogen3){
-    return console.log(nitrogen1+nnitrogen2+nitrogen3);
-}
-function(execSomth){
-    var FA={};
-}
+'use strict';
+
