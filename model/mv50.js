30adb486a35d74effdf18d44906948065fd94022
D0m1nator <expirioner@gmail.com>
2014-02-25 18:02:24 +0700

    >>Double Banch Merging

--- model/mv50.js
+++ model/mv50.js
@@ -1,130 +1,15 @@
-//function assert(callBackParam, stringParam){
-//
-//}
-//
-////String definition
-//var textCall='Domo Dominatus';
-///**
-// * Asert functions retrive function as parametr and text string with paramer
-// */
-//assert(
-//    //Useless function as argument
-//    useless(
-//        //Callback function as argument
-//        function(){
-//            return text;
-//        }
-//    )===text, 'The useless function works!!!' + text//END
-//);
-//
-//var arrayToSort=[213, 16, 2058, 54, 10, 1965, 57, 9];
-//sortedArray=arrayToSort.sort();
-//console.log(sortedArray);
-//
-////Function naming order assertion
-//function isNimble(){
-//    return true;
-//}
-//
-//assert(typeof window.isNimble()==="function", "isNimmbe defined" );
-//assert(isNimble().name==='isNimble', 'isNimble has a name');
-//
-//var canFly=function(){
-//    return true;
-//};
-//
-//canFly();
-//
-//assert(typeof  window.canFly()==='function', 'canfly DEFINED');
-//assert(canFly().name==='', 'can fly has no name');
-//
-//
-//
-//window.isDeadly =function(){
-//    return true;
-//};
-//
-//assert(typeof window.isDeadly()==='function');
-//function growth(paramZCHAIN){
-//    var chain=[],
-//        intNumberofConnectors= 5,
-//        strAtomType="vanadium";
-//    console.log(chain+intNumberofConnectors+ strAtomType + paramZCHAIN);
-//
-//}
-//                assert(typeof inner()==="function",
-//                'inner() in scope after declaration';)
-//                assert(window.inner===undefined, 'inner() in not global scope');
-//}
-//var nitrogen;
-//switch (nitrogen){
-//    case 1:
-//        console.log('basic mixture');
-//        break;
-//    case 2:
-//        console.log('more advansed mixture');
-//        break;
-//
-//outer();
-//assert(window.inner === undefined, "inner still not in global scope");
-//
-//window.wieldsSword = function swingSword() {
-//    return true;
-//};
-//assert(window,wieldsSword().name==='swingsSword', 'wieldSword - real name is swingsSword');
-
-document.writeln('Domo Dominatus');
-
-Function.prototype.method=function(name, func){
-    this.prototype[name]=func;
-    return this;
-};
-
-var partedVarible;
-partedVarible = .9;
-addedPartVariable= partedVarible + .781;
-console.log(typeof partedVarible);
-//Exponental integer variables
-var exponentalVariable= 3e2,
-    addedExponentals=exponentalVariable+ .42e4;
-
-var RussianExpiriment= 672 +-67,
-    Expiriment=67-+567;
-
-metod(gUnit,function(powerToThePeople){
-    return "fight the power";
-});
-
-methodMan =array.prototype=function(){
-  return 'cat' === 'c' + 'a' + 't';
-};
-
-let yui=0;
-
-multiply: if (!nitrogen) {
-    return 2;
-
-} else {
-    return 1;
-};
-
-if (1===true) {return yui;}
-
-switch (yui){
-    case 1:
-        return "yui equals 1";
-    break;
-
-    case 2:
-        return "yui equals 2";
-    break;
-    default:
-        return "nothing interesting";
+(function(){
+    function swingSword(swordType, powerOfStrike){
+    console.log(swordType + ' '+ powerOfStrike);
+    return "strike performed " + swordType;
 }
 
+var strikeChain=[
+    x=swingSword('blade', 100),
+    y=swingSword('axe', 200),
+    z=swingSword('chainsaw', 100)
+];
+console.log("fakir #{strikeChain}");
+console.log(strikeChain);
 
-do{
-    var i=1;
-    i++;
-    console.log('do cycle');
-}while(i<20);
+})();
